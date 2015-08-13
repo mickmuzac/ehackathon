575e63daacf53c63ee4a9fbf79b878bf751aab10
Mick Muzac <mmuzac23@gmail.com>
2015-08-12 21:15:07 -0400

    Very minor updates towards the inclusion of Angular

--- ehackathon-server/public/js/app.js
+++ ehackathon-server/public/js/app.js
@@ -1,7 +1,9 @@
 var app = angular.module("weekendmvp", []);
 
-app.controller("BaseController", ['$scope', function($scope){
+app.controller("BaseController", ['$scope', '$http', function($scope, $http){
   //Add fundamental stuff to app here...
   $scope.myTeams;
+  $http.get('/teams/...') // <--- delete me or entire file
+
 
 }]);
--- ehackathon-server/routes/teams.js
+++ ehackathon-server/routes/teams.js
@@ -6,8 +6,7 @@ var dal = require('../models/dal');
 router.get('/', ensureAuthenticated, function(req, res, next) {
   dal.findTeamsByMemberId(req.user._id, function(err, doc){
     res.render('teams', {
-      title: 'Teams',
-      teams: doc
+      title: 'Teams'
     });
   });
 });
