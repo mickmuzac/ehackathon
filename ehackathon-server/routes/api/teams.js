99ac33fd8fae6d0e26f4f373751c16780e608e37
Mick Muzac <mmuzac23@gmail.com>
2015-08-12 23:35:29 -0400

    Added user invites endpoints to api

--- ehackathon-server/routes/api/teams.js
+++ ehackathon-server/routes/api/teams.js
@@ -53,5 +53,30 @@ router.get('/teams/invite/code', ensureAuthenticated, function(req, res, next) {
   })
 });
 
+router.get('/teams/invite/redeem/:codeId', ensureAuthenticated, function(req, res, next) {
+  //TODO: Add validation(does user already belong to/own team etc..., clean up the nestedness
+  var code = dal.findInviteById(req.query.codeId, function(err, doc) {
+    if(err === null && doc !== null) {
+
+      dal.addUserToTeam(doc.teamId, req.user._id, function(err, doc) {
+        if(err === null) {
+          dal.deleteTeamInvite(req.query.codeId, function(err) {
+            if(err === null) {
+              res.send(200);
+            } else {
+              res.send(500, err);
+            }
+          })
+        } else {
+          res.send(500, err);
+        }
+
+      })
+    } else {
+      res.send(404);
+    }
+  })
+})
+
 
 module.exports = router;
